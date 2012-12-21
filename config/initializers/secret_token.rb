c60b4168234c4fd1d6c23c3d4a088b470815cc6d
Karl Smith <threadhead@gmail.com>
2012-12-21 13:36:17 -0700

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplexNestedForm::Application.config.secret_token = '575ba524e13905b62f245f657b88a1ccb6c6ab33b64654f2f66df650987a8ae0cc581dbf806ed49ed4e79b0965169f33c8b63004ca5b305532a9f71a985dd9c4'
