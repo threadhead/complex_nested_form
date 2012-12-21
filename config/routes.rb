0d652128e99b25970b76bd6c71eda309cd821450
Karl Smith <threadhead@gmail.com>
2012-12-21 13:46:02 -0700

    conjure up some models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ComplexNestedForm::Application.routes.draw do
+  resources :sub_units
+
+
+  resources :organizations
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
